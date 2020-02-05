5e8306a824482ee7084b810ce109fd59379ccb4a
ez-li <eileenli.sf@gmail.com>
2020-02-05 00:44:16 -0800

    Updating App into a Class Component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import Header from './Header.js';
 import Form from './Form.js';
 import Preview from './Preview.js';
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
 
-      <main className="app-main">
-        <section className="app-left">
-          <Form />
-        </section>
-        <section className="app-right">
-          <Preview />
-        </section>
-      </main>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Header />
+  
+        <main className="app-main">
+          <section className="app-left">
+            <Form />
+          </section>
+          <section className="app-right">
+            <Preview />
+          </section>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
