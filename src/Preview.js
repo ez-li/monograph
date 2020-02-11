bc987b0eafa34d0565f3eab91b33e49eb6450f05
ez-li <eileenli.sf@gmail.com>
2020-02-11 12:06:17 -0800

    More CSS styling optimization for mobile

--- src/Preview.js
+++ src/Preview.js
@@ -8,29 +8,31 @@ function Preview(props) {
   return (
     <section className="preview">
       <div className="preview-label">Preview of your review</div>
-      <div className="preview-content">
-        <div className="preview-rating">
-        {selectedRating}
+      <div className="preview-area">
+        <div className="preview-content">
+          <div className="preview-rating">
+          {selectedRating}
+          </div>
+          <div className="preview-feedback">
+            {feedback === '' ? 
+              <div className="preview-sample-feedback">Let us know what we did well or could improve...</div>
+              : feedback}
+          </div>
         </div>
-        <div className="preview-feedback">
-          {feedback === '' ? 
-            <div className="preview-sample-feedback">Let us know what we did well or could improve...</div>
-            : feedback}
-        </div>
+        <div className="preview-bottomBar">
+            <div className="preview-party">
+              {partySize} people dined here.
+            </div>
+            <div className="preview-emoji-tags-holder">          
+              {selectedTags.map((emoji, i) => (
+                <div className='preview-emoji-tag'
+                  key={i}>
+                  {emoji}
+                </div>
+              ))}
+            </div>
+          </div>
       </div>
-      <div className="preview-bottomBar">
-          <div className="preview-party">
-            {partySize} people dined here.
-          </div>
-          <div className="preview-emoji-tags-holder">          
-            {selectedTags.map((emoji, i) => (
-              <div className='preview-emoji-tag'
-                key={i}>
-                {emoji}
-              </div>
-            ))}
-          </div>
-        </div>
     </section>
   );
 }
